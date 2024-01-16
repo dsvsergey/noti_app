d01d141c2dff55c87ca1ba989adb0d4a11e11b14
Serhii Ddobrovolskyi <serhii.dobrovolskyi@dipdev.studio>
2024-01-16 13:05:39 +0200

    Added routers

--- lib/core/app_route.dart
+++ lib/core/app_route.dart
@@ -1,4 +1,6 @@
 class AppRoute {
   static const String home = '/';
   static const String login = '/login';
+  static const String notification = '/notification';
+  static const String selectTrigger = '/select-trigger';
 }
